302818374c70962f430abc9b374deb6176378dd6
Olga Engels <ovengels@yahoo.com>
2017-03-24 14:57:08 -0500

    Adds new pets

---
+++ server/modules/pets.js
@@ -0,0 +1,41 @@
+var express = require('express');
+var router = express.Router();
+var pg = require('pg');
+
+var config = {
+  database: 'chi', // name of your database
+  host: 'localhost', // where is your database?
+  port: 5000, // port for the database
+  max: 10, // how many connections at one time
+  idleTimeoutMillis: 30000 // 30 seconds to try to connect
+};
+
+var pool = new pg.Pool(config);
+
+
+router.post('/add', function(req, res){
+  console.log(req.body);
+  var name = req.body.name;
+  var  color = req.body.color;
+  var breed = req.body.breed;
+  // INSERT INTO "books" ("author", "title") VALUES ('David Mitchel','Cloud Atlas');
+  pool.connect(function(errorConnectingToDatabase, db, done){
+    if(errorConnectingToDatabase) {
+      console.log('Error connecting to the database.');
+      res.send(500);
+    } else {
+      // We connected!!!!
+      db.query('INSERT INTO "pets" ("name", "color", "breed")' +
+               ' VALUES ($1,$2,$3);',
+               [name, color, breed], function(queryError, result){
+        done();
+        if(queryError) {
+          console.log('Error making query.');
+          res.send(500);
+        } else {
+          res.sendStatus(201);
+        }
+      });
+    }
+  });
+});
--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,4 +1,72 @@
 
 $(document).ready(function(){
   console.log('jQ totes sourced!');
+
+  getPets();
+  $('#petInput').on('submit', function(event){
+    event.preventDefault();
+    console.log($('#petName').val(), $('#petColor').val(), $('#petBreed').val());
+
+    $.ajax({
+      type: "POST",
+      url: "/pets/add",
+      data: {name: $('#petName').val(), color:$('#petColor').val(), breed:$('#petBreed').val()},
+      success: function(response) {
+        getPets();
+      }
+    });
+  });
 });
+
+function getPets() {
+  $.ajax({
+    type: 'GET',
+    url: '/pets',
+    success: function(response) {
+      console.log(response);
+      // displayPets(response);
+    }
+  });
+}
+
+//
+// Owner Name<input type="text" id="ownerSelect">
+// Pet Name<input type="text" id="petName">
+// Color<input type="text" id="petColor">
+// Breed<input type="text" id="petBreed">
+// <button type="button" id="addPet">Add Pet</button>
+//
+// function getPets() {
+// $.ajax({
+// type: "GET",
+// url: "/pets",
+// success: function(response) {
+// console.log(response);
+// $('#getPets').empty();
+// for(var i = 0; i < response.length; i++) {
+//   var book = response[i];
+//   $('#books').append('<tr></tr>');
+//   var $el = $('#books').children().last();
+//   $el.append('<td>' + book.id + '</td>');
+//   $el.append('<td>' + book.author + '</td>');
+//   $el.append('<td>' + book.title + '</td>');
+//   $el.append('<td>' + book.publisher + '</td>');
+//   $el.append('<td>' + book.year + '</td>');
+// }
+// }
+// });
+// }
+//
+// function displayPets(pets) {
+//   $("#outputDiv").empty();
+//   for(var i = 0; i < pets.length; i++) {
+// // console.log(pets[i]);
+//     $("#outputDiv").append("<span>" +
+//                     pets[i].whoseJoke +
+//                     ": " +
+//   pets[i].jokeQuestion + ": " + pets[i].punchLine +
+//                                "</span><br>");
+//   }
+// }
+
+//

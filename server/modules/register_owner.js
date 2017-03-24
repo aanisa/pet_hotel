9e60d5fe99845c9259d0630b662faa1e916605e3
Anisa Abdulkadir <anisa.ali7@gmail.com>
2017-03-24 15:25:00 -0500

    comment for adding database info

--- server/modules/register_owner.js
+++ server/modules/register_owner.js
@@ -2,18 +2,19 @@ var express = require('express');
 var router = express.Router();
 var pg = require('pg');
 
-// var config = {
-//     database: 'chi',
-//     host: 'localhost',
-//     port: 5432,
-//     max: 10,
-//     idleTimeOutMillis: 3000
-// };
-//
-// var pool = new pg.Pool(config);
+var config = {
+    database: 'chi',
+    host: 'localhost',
+    port: 5432,
+    max: 10,
+    idleTimeOutMillis: 3000
+};
+
+var pool = new pg.Pool(config);
 
 router.post('/', function(req, res) {
     res.send("from router woot woot");
+    //add db.pool info to get database information
 });
 
 module.exports = router;

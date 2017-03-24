358a42a2c22656c71f9ee5813359030b4cf84593
Anisa Abdulkadir <anisa.ali7@gmail.com>
2017-03-24 15:08:08 -0500

    redo all changes

--- server/modules/register_owner.js
+++ server/modules/register_owner.js
@@ -1,16 +1,16 @@
 var express = require('express');
 var router = express.Router();
-// var pg = require('pg');
-//
-// var config = {
-//     database: 'chi',
-//     host: 'localhost',
-//     port: 5432,
-//     max: 10,
-//     idleTimeOutMillis: 3000
-// };
-//
-// var pool = new pg.Pool(config);
+var pg = require('pg');
+
+var config = {
+    database: 'chi',
+    host: 'localhost',
+    port: 5432,
+    max: 10,
+    idleTimeOutMillis: 3000
+};
+
+var pool = new pg.Pool(config);
 
 router.post('/', function(req, res) {
     res.send("from router woot woot");

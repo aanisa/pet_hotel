a65d0a6cce5d00476a66a14bbbed5b3193be1ba1
Dan Zera <dczera@gmail.com>
2017-03-24 14:01:14 -0500

    add event listeners function and call that function in document ready, start click listener for udpate button - will need additional changes based on what the button creation looks like

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,4 +1,33 @@
 
 $(document).ready(function(){
   console.log('jQ totes sourced!');
+
+  // call addEventListeners() function
+  addEventListeners();
 });
+
+// main function for all event listeners
+function addEventListeners() {
+  // add on.click listener for update "go" button
+  // -- may want to add an ID to <table> and change this
+  // to target the table ID -- this listener assumes the update
+  // buttons will have a class of "update" (change accordingly if needed)
+  $('table').on('click', '.update', function() {
+    // assumes an id is attached to the button when it is created
+    var id = $(this).data.id;
+    // make AJAX call to server
+    $.ajax({
+      type: 'PUT',
+      url: '/update/:' + id, // send pet id as a variable in the url
+      data: {
+        owner: $(this).data.owner,// may not be editable?
+        pet: $(this).data.pet,
+        breed: $(this).data.breed,
+        color: $(this).data.color
+      },
+      success: function() {
+        console.log('our update call to the server was successful');
+      }
+    }); // end ajax call
+  }); // end click listener for table-update button
+}
